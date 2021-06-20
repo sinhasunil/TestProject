6240903077498bb8079394bc0a407caeed79bc70
sinhasunil <sinha.sunil3@rediffmail.com>
2021-06-20 14:25:28 +0530

    addstring

---
+++ JavaCoreProject/src/com/java/AddString.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class AddString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a  =  "Ram" ;
+		
+		String b  =  "Sita" ;
+		
+		
+		String c  =  a +b ;
+		
+		System.out.println(c);
+		 
+	}
+
+}
